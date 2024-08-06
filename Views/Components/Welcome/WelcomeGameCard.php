e70a11d5c0d8a61eeee2f4434b3c88ee649f5609
Carlos Atílio Manuel <72392949+CarlosAtilioManuel@users.noreply.github.com>
2024-08-06 07:41:05 +0100

    updating others
    let me do this

---
+++ Views/Components/Welcome/WelcomeGameCard.php
@@ -0,0 +1,5 @@
+<div class="welcome-aviator-game-card">
+    <img src="../../../public/img/aviator-game-card.png" alt="">
+
+    <button>Login</button>
+</div>
--- Views/Modules/Welcome/body.php
+++ Views/Modules/Welcome/body.php
@@ -14,9 +14,25 @@
         </div>
     </menu>
 
-    <section>
+    <section class="welcome-slide-section">
         <div class="welcome-slide">
             <img src="../../../public/img/aviator-welcome-page-slide-image-1.jpg" alt="">
+        </div>
+    </section>
+
+    <section class="welcome-card-section">
+        <div class="welcome-card-title">
+            <h1>Our Games</h1>
+        </div>
+
+        <div class="welcome-cards">
+            <?php
+                $counter = 0;
+                
+                for($counter = 0; $counter < 5; $counter++){
+                    include_once 'Views/Components/Welcome/WelcomeGameCard.php';
+                }
+            ?>
         </div>
     </section>
 </main>
